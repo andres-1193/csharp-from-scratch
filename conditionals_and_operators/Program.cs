4ec5574f290af78e8c5ef15f40b4369c916befca
ANDRES MAURICIO TUBERQUIA CAMPO <andrestuberquia213089@correo.itm.edu.co>
2024-03-19 01:02:15 -0500

    cambios operadores

---
+++ conditionals_and_operators/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Console.WriteLine("Ingrese el numero");
+int num = int.Parse(Console.ReadLine());
+if(num>0){
+    Console.WriteLine("Positivo");
+    else if  Console.WriteLine("Negativo");
+    else  Console.WriteLine("es Cero");*/
+    
+        
+    
+//operadores
+Console.WriteLine("Ingrese el primer  numero");
+double a =double.Parse(Console.ReadLine());
+Console.WriteLine("Ingrese el segundo numero");
+double b =double.Parse(Console.ReadLine());
+const string Indeterminado ="indeterminado";
+
+Console.WriteLine($"suma es:{a+b} la resta es :{a-b} la multipliccacion {a*b} la divicion {( b!=0 ? a/b : Indeterminado)}");
+
+
+
+
